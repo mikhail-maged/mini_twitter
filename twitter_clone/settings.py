7be1a03e98d66dae7d5b38f9f928cfc98ec3270b
mikail-maged <mkhmaged@gmail.com>
2022-08-26 05:10:23 +0200

    testagain test

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 from datetime import timedelta
 import dj_database_url
+import django_on_heroku
+import django_extensions
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
